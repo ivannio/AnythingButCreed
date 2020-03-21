8e33a3fc6c3633d3cc387a339a67565dd8dbdfab
Ivan Phelps <ivanniothemannio@gmail.com>
2020-03-21 12:26:29 -0500

    Add project files.

---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allSongs = new List<Song>();
+
+            allSongs.Add(new Song("Creed", "With Arms Wide Open"));
+            allSongs.Add(new Song("Creed", "Higher"));
+            allSongs.Add(new Song("Gorillaz", "Welcome To The World of the Plastic Beach"));
+            allSongs.Add(new Song("Gorillaz", "White Flag"));
+            allSongs.Add(new Song("Gorillaz", "Rhinestone Eyes"));
+            allSongs.Add(new Song("Gorillaz", "Stylo"));
+            allSongs.Add(new Song("Gorillaz", "Superfast Jellyfish"));
+            allSongs.Add(new Song("Gorillaz", "Welcome To The World of the Plastic Beach"));
+            allSongs.Add(new Song("Gorillaz", "Empire Ants"));
+            allSongs.Add(new Song("Gorillaz", "Glitter Freeze"));
+            allSongs.Add(new Song("Gorillaz", "Some Kind of Nature"));
+            allSongs.Add(new Song("Gorillaz", "On Melancholy Hill"));
+
+            var goodSongs = new List<Song>();           
+            
+            goodSongs.AddRange(allSongs.Where(song => song.Artist != "Creed"));            
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist}: {song.Title}");
+            }            
+        }
+    }
+}
---
+++ AnythingButCreed/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnythingButCreed
+{
+    class Song
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+
+        public Song(string artist, string title)
+        {
+            Artist = artist;
+            Title = title;
+        }
+    }
+}
